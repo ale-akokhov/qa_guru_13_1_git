3b367fd686102db4fed15232c5ca922668a1fdbb
Alexandr Akokhov <aleakokhov75@gmail.com>
2026-01-04 14:22:59 +0300

    refactoring

--- src/test/java/guru/qa/FirstTest.java
+++ src/test/java/guru/qa/FirstTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 public class FirstTest {
     @Test
     void firstTest() {
-        Assertions.assertFalse(3 < 2);
+        Assertions.assertFalse(3 < 2); // comment
     }
 
     @Test
